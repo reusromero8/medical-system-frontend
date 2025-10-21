8a7ac29b102926e2b41c00e09ee0ed771d1a9164
J0SAN <jose22ekprogo@gmail.com>
2025-10-21 00:33:15 -0500

    feat: Se configuraron las rutas de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,52 @@ import { Routes } from '@angular/router';
 import { PatientComponent } from './pages/patient-component/patient-component';
 import { PatientEditComponent } from './pages/patient-component/patient-edit-component/patient-edit-component';
 import { MedicComponent } from './pages/medic-component/medic-component';
+import { ProductComponent } from './pages/product/product';
+import { ProductEditComponent } from './pages/product-edit/product-edit';
 
 export const routes: Routes = [
-    { path: 'pages/patient', component: PatientComponent,
-        children: [
-            { path: 'new', component: PatientEditComponent },
-            { path: 'edit/:id', component: PatientEditComponent }
-        ]
+    // Rutas de pacientes
+    { 
+        path: 'pages/patient', 
+        component: PatientComponent 
     },
-    { path: 'pages/medic', component: MedicComponent }
+    { 
+        path: 'pages/patient/new', 
+        component: PatientEditComponent 
+    },
+    { 
+        path: 'pages/patient/edit/:id', 
+        component: PatientEditComponent 
+    },
+
+    // Rutas de médicos
+    { 
+        path: 'pages/medic', 
+        component: MedicComponent 
+    },
+
+    // Rutas de productos
+    { 
+        path: 'products', 
+        component: ProductComponent 
+    },
+    { 
+        path: 'products/new', 
+        component: ProductEditComponent 
+    },
+    { 
+        path: 'products/edit/:id', 
+        component: ProductEditComponent 
+    },
+
+    // Redirecciones
+    { 
+        path: '', 
+        redirectTo: 'pages/patient', 
+        pathMatch: 'full' 
+    },
+    { 
+        path: '**', 
+        redirectTo: 'pages/patient' 
+    }
 ];
